bcabe746711309cb0270c34fa80b8ca627fff54a
Hamidou Diallo <dio.hamidou@gmail.com>
2019-09-24 02:38:46 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -30,7 +30,7 @@ def monopoly_with_third_tier
 	   pieces:4,
 	   rent_in_dollars:{
 	    one_piece_owned: 25,
-	    two_piece_owned: "",
+	    two_piece_owned: 50,
 	    three_piece_owned: "",
 	    four_piece_owned: ""
 	   },
