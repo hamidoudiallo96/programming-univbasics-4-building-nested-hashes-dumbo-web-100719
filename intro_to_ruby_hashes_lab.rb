c8afe0c7b2739458178c8e53e239829847720ca4
Hamidou Diallo <dio.hamidou@gmail.com>
2019-09-24 02:23:50 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -1,7 +1,10 @@
 def base_hash
   # Remember implicit return! We're returning this new Hash _without_ the
   # keyword "return." Nice and neat.
-	{}
+	monopoly = {
+	 railroads:{}
+	 }
+	 return monopoly
 end
 
 def monopoly_with_second_tier
@@ -9,6 +12,12 @@ def monopoly_with_second_tier
   # you built in the previous method. Run the tests. They will fail, but
   # they'll guide you in how to modify what you just did to have more
   # complexity.
+  monopoly = {
+	 railroads:{
+	   pieces:4
+	  }
+	 }
+	 return monopoly
 end
 
 def monopoly_with_third_tier
@@ -16,6 +25,20 @@ def monopoly_with_third_tier
   # you built in the previous method. Run the tests. They will fail, but
   # they'll guide you in how to modify what you just did to have more
   # complexity.
+  monopoly = {
+	 railroads:{
+	   pieces:4,
+	   rent_in_dollars:{
+	     names:{
+	      names:""
+	     },
+	     first_name: "",
+	     second_name: "",
+	     third_name: ""
+	   }
+	  }
+	 }
+  return monopoly
 end
 
 def monopoly_with_fourth_tier
