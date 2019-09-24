9b580b81b79daa561693172a55c3eafb7a310600
Hamidou Diallo <dio.hamidou@gmail.com>
2019-09-24 02:37:25 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -29,7 +29,13 @@ def monopoly_with_third_tier
 	 railroads:{
 	   pieces:4,
 	   rent_in_dollars:{
-	    
+	    one_piece_owned: "",
+	    two_piece_owned: "",
+	    three_piece_owned: "",
+	    four_piece_owned: ""
+	   },
+	   names:{
+	     
 	   }
 	  }
 	 }
